4cddaff0803b82053e934d51c2083e36dc8eaf0c
Scott Walters <srwalte2@illinois.edu>
2013-11-15 22:51:54 -0600

    Fixed a bug when adding multiple events
    It now doesn't horribly break everything. Hooray!

--- public/js/bettit.js
+++ public/js/bettit.js
@@ -1,19 +1,42 @@
-var event_title_html = $("<input>", {
-	'id'          : 'add_event_title',
-	'class'       : 'input-block-level',
-	'type'        : 'text',
-	'placeholder' : 'Event title'
-}).attr('required', 'required');
 var outcome_html = $("<input>", {
 	'class'       : 'input-block-level add_event_outcome',
 	'type'        : 'text',
 	'placeholder' : 'Outcome'
 });
-var submit_button_html = $("<input>", {
-	'type'  : 'submit',
-	'class' : 'btn btn-primary',
-	'value' : 'Add Event'
+/**
+ * When a mod types into the last outcome input
+ */
+$("#add_event_span").on("keypress", ".add_event_outcome:last", function(){
+	$(this).after(outcome_html.clone().val(''));
 });
+/**
+ * When a mod submits a new event
+ */
+$("#add_event_span").on("submit", "form#add_event_form", function(event){
+	event.preventDefault();
+	var eventTitle = $("#add_event_title").val();
+	console.log(eventTitle);
+	$("#add_event_title").val('');
+	var $outcomes = $('.add_event_outcome');
+	var outcomes = [];
+	$outcomes.each(function(){
+		var value = $(this).val();
+		$(this).val('');
+		if(value !== '') outcomes.push(value);
+	})
+	io.emit('add_event', {
+		title    : eventTitle,
+		outcomes : outcomes
+	});
+	addAddEventButton();
+})
+
+function addAddEventButton(){
+	$('#add_event_span').html('' + '<button ' +
+		'id="add_event_button" ' +
+		'class="btn btn-large btn-block btn-primary"' +
+		' type="button">Add event</button><br/>');
+}
 var mod = false;
 
 $(document).ready(function(){
@@ -40,10 +63,7 @@ $(document).ready(function(){
 	});
 	io.on('is_mod_response', function(data){
 		if(!mod) {
-			$('#add_event_span').html('' + '<button ' +
-				'id="add_event_button" ' +
-				'class="btn btn-large btn-block btn-primary"' +
-				' type="button">Add event</button><br/>');
+			addAddEventButton();
 			mod = true;
 		}
 	});
@@ -79,47 +99,25 @@ $(document).ready(function(){
 	})
 
 	/**
-	 * Handle when any event form is submitted (bet on)
-	 */
-	$(".event_form").each(function(index){
-		var $form = $(this);
-		$form.submit(function(event){
-			event.preventDefault();
-			var id = $form.attr('id');
-			var search = '#' + id + ' input:checked';
-			$(search).each(function(index){
-				console.log($(this));
-			})
-		});
-	});
-
-	/**
 	 * Handles a moderator adding an event
 	 */
 	$(document).on("click", "#add_event_button", function(){
+		var event_title_html = $("<input>", {
+			'id'          : 'add_event_title',
+			'class'       : 'input-block-level',
+			'type'        : 'text',
+			'placeholder' : 'Event title'
+		}).attr('required', 'required');
+		var submit_button_html = $("<input>", {
+			'type'  : 'submit',
+			'class' : 'btn btn-primary',
+			'value' : 'Add Event'
+		});
+
 		$("#add_event_span").html($("<div>",
 			{'class' : 'well well-small'}).append($("<form>",
 				{'id' : 'add_event_form'}).append(event_title_html)
 				.append(outcome_html).append(outcome_html.clone().val(''))
 				.append(submit_button_html)));
-
-		$("#add_event_span").on("keypress", ".add_event_outcome:last", function(){
-			$(this).after(outcome_html.clone().val(''));
-		});
-
-		$("#add_event_span").on("submit", "form#add_event_form", function(event){
-			event.preventDefault();
-			var eventTitle = $("#add_event_title").val();
-			var $outcomes = $('.add_event_outcome');
-			var outcomes = [];
-			$outcomes.each(function(){
-				var value = $(this).val();
-				if(value !== '') outcomes.push(value);
-			})
-			io.emit('add_event', {
-				title    : eventTitle,
-				outcomes : outcomes
-			});
-		})
-	})
+	}) // onClick() #add_event_button
 });
