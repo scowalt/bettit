5fefc94e8d0274983cca2adaec71da25d306e799
Scott Walters <srwalte2@illinois.edu>
2013-12-05 18:01:48 -0600

    Cleaned up user page
    Also fixed a bug when looking up a user not the DB would crash the
    server

--- web.js
+++ web.js
@@ -405,6 +405,7 @@ app.get('/user/:username', function(req, res){
 function userFunction(req,res){
 	db.User.find({where : {username: req.params.username}}).success(
 		function onSuccess(user){
+			if (!user) return; // TODO Serve special page for user not found
 			res.render('user', {
 				user : user.values.username,
 				money : user.values.money
