0dbda2e2b09dbe43d2b9e2c66d4f8ace598770a3
Scott Walters <srwalte2@illinois.edu>
2013-12-07 16:44:10 -0600

    Refactored bet socket handler

--- sockets/index.js
+++ sockets/index.js
@@ -1,7 +1,9 @@
 var ready = require(__dirname + '/ready');
 var addEvent = require(__dirname + '/addEvent');
+var bet = require(__dirname + '/bet');
 
 module.exports = {
 	addEvent : addEvent,
+	bet : bet,
 	ready : ready
 };
